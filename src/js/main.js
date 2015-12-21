ba51f4018c1ee9d270bddd421b7ccc662502213e
terehov <terehovsergej@gmail.com>
2015-12-21 20:07:50 +0200

    in process

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,23 @@ var app = app || {};
 app.events = {
     init: function() {
         this.projectsItems();
+        this.projectDescrip();
     },
     projectsItems: function () {
         var $item = $('.project-item');
         if ($item.length > 0) {
             $item.css('height', $item.width());
+        }
+    },
+    projectDescrip: function () {
+        var $projDescrip = $('.project-descrip');
+        if ($projDescrip.length > 0) {
+            $projDescrip.scrollToFixed({
+                marginTop: 5,
+                limit: function () {
+                    return $('footer').offset().top - $(this).height();
+                }
+            });
         }
     }
 };
@@ -21,7 +33,6 @@ var App = (function($, app){
 $(function () {
     App.init();
 });
-
 $(window).resize(function () {
     app.events.projectsItems();
 });
